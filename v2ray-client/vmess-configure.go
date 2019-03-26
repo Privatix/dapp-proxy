4c82057212c55e417b22df084ce43e27c55a5e75
furkhat <vailodf@gmail.com>
2019-03-26 17:09:44 +0600

    configure vmess on client

--- v2ray-client/vmess-configure.go
+++ v2ray-client/vmess-configure.go
@@ -13,11 +13,15 @@ import (
 	"v2ray.com/core/common/net"
 	"v2ray.com/core/common/protocol"
 	"v2ray.com/core/common/serial"
+	"v2ray.com/core/proxy/freedom"
 	"v2ray.com/core/proxy/vmess"
 	"v2ray.com/core/proxy/vmess/outbound"
 )
 
-const outboundVmessTag = "vmess-outbound"
+const (
+	outboundVmessTag   = "outbound"
+	outboundDefaultTag = "outbound-default"
+)
 
 // Configurer configures outbounds on client.
 type Configurer struct {
@@ -31,16 +35,33 @@ func NewConfigurer(conn *grpc.ClientConn) *Configurer {
 	}
 }
 
-// AddVmessRequest params to configure vmess outbound.
-type AddVmessRequest struct {
+// VmessOutbound params to configure vmess outbound.
+type VmessOutbound struct {
 	Address string
 	AlterID uint32
 	ID      string
 	Port    uint32
 }
 
-// AddVmess configures vmess outbound.
-func (c *Configurer) AddVmess(ctx context.Context, req *AddVmessRequest) error {
+// ConfigureVmess configures vmess outbound.
+func (c *Configurer) ConfigureVmess(ctx context.Context, req *VmessOutbound) error {
+	err := c.addVmessOutbound(ctx, req)
+	if err != nil {
+		return err
+	}
+	return c.removeOutbound(ctx, outboundDefaultTag)
+}
+
+// RemoveVmess removes vmess outbound.
+func (c *Configurer) RemoveVmess(ctx context.Context) error {
+	err := c.addDefaultOutbound(ctx)
+	if err != nil {
+		return err
+	}
+	return c.removeOutbound(ctx, outboundVmessTag)
+}
+
+func (c *Configurer) addVmessOutbound(ctx context.Context, req *VmessOutbound) error {
 	addr, err := parseAddr(req.Address)
 	if err != nil {
 		return fmt.Errorf("could not parse ip address %s: %v", req.Address, err)
@@ -69,10 +90,19 @@ func (c *Configurer) AddVmess(ctx context.Context, req *AddVmessRequest) error {
 	return err
 }
 
-// RemoveVmess removes vmess outbound.
-func (c *Configurer) RemoveVmess(ctx context.Context) error {
+func (c *Configurer) addDefaultOutbound(ctx context.Context) error {
+	_, err := c.handler.AddOutbound(ctx, &command.AddOutboundRequest{
+		Outbound: &core.OutboundHandlerConfig{
+			Tag:           outboundDefaultTag,
+			ProxySettings: serial.ToTypedMessage(&freedom.Config{}),
+		},
+	})
+	return err
+}
+
+func (c *Configurer) removeOutbound(ctx context.Context, tag string) error {
 	_, err := c.handler.RemoveOutbound(ctx, &command.RemoveOutboundRequest{
-		Tag: outboundVmessTag,
+		Tag: tag,
 	})
 	return err
 }

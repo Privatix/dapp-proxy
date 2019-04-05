a7b89d5066c18d92bbdcfd5a03a7b2369ac19d26
furkhat <vailodf@gmail.com>
2019-04-05 20:09:43 +0600

    build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
 MY_PATH="`dirname \"$0\"`" # relative bash file path
-DAPP_PROXY_DIR="`( cd \"$MY_PATH/..\" && pwd )`"  # absolutized and normalized dappctrl path
+DAPP_PROXY_DIR="`( cd \"$MY_PATH/..\" && pwd )`"  # absolutized and normalized dapp-proxy path
 
 GIT_COMMIT=$(git rev-list -1 HEAD)
 GIT_RELEASE=$(git tag -l --points-at HEAD)
+# if $GIT_RELEASE is zero:
+GIT_RELEASE=${GIT_RELEASE:-$(git rev-parse --abbrev-ref HEAD | grep -o "[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}")}
+
+echo
+echo GIT_COMMIT=${GIT_COMMIT}
+echo GIT_RELEASE=${GIT_RELEASE}
 
 cd "${DAPP_PROXY_DIR}"
 

98648ad6bcd730b8aac0c124a915f87b10f9bd85
Andrei Tseitlin <sofabeat@gmail.com>
2019-06-14 16:30:23 +0300

    GIT_COMMIT missed

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,13 @@ MY_PATH="`dirname \"$0\"`" # relative bash file path
 DAPP_PROXY_DIR="`( cd \"$MY_PATH/..\" && pwd )`"  # absolutized and normalized dapp-proxy path
 
 GIT_COMMIT=$(git rev-list -1 HEAD | head -n 1)
-GIT_RELEASE=$(git tag -l --points-at HEAD | head -n 1)
-
-# if $GIT_RELEASE is zero:
-GIT_RELEASE=${GIT_RELEASE:-$(git rev-parse --abbrev-ref HEAD | grep -o "[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}")}
+if [ -z ${VERSION_TO_SET_IN_BUILDER} ]; then
+    GIT_RELEASE=$(git tag -l --points-at HEAD | head -n 1)
+    # if $GIT_RELEASE is zero:
+    GIT_RELEASE=${GIT_RELEASE:-$(git rev-parse --abbrev-ref HEAD | grep -o "[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}")}
+else
+    GIT_RELEASE=${VERSION_TO_SET_IN_BUILDER}
+fi
 
 echo
 echo GIT_COMMIT=${GIT_COMMIT}
